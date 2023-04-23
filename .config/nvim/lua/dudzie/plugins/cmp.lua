12ae4cd00adf7b31ec684abe7d6d77c2e08aa17a
id-b3 <58172188+id-b3@users.noreply.github.com>
2023-04-23 20:44:04 +0200

    Autocomplete functional with snippets, lsp and copilot!

--- .config/nvim/lua/dudzie/plugins/cmp.lua
+++ .config/nvim/lua/dudzie/plugins/cmp.lua
@@ -6,6 +6,16 @@ local M = {
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-cmdline",
         "hrsh7th/cmp-path",
+        "saadparwaiz1/cmp_luasnip",
+        {"L3MON4D3/LuaSnip",
+            dependencies = {
+                {"rafamadriz/friendly-snippets",
+                    config = function()
+                        require("luasnip.loaders.from_vscode").lazy_load()
+                    end
+                }
+            }
+        },
         {
             "zbirenbaum/copilot-cmp",
             after = {"copilot.lua"},
@@ -27,20 +37,15 @@ local M = {
           return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
         end
 
-        -- local luasnip = require("luasnip")
+        local luasnip = require("luasnip")
 
         return {
-            -- snippet = {
-            --     expand = function(args)
-            --         luasnip.lsp_expand(args.body)
-            --     end,
-            -- },
             snippet = {
-                expand = function() end,
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
             },
             mapping = cmp.mapping.preset.insert({
-                ["<C-p>"] = cmp.mapping.select_prev_item(),
-                ["<C-n>"] = cmp.mapping.select_next_item(),
                 ["<C-d>"] = cmp.mapping.scroll_docs( -4),
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
                 ["<C-Space>"] = cmp.mapping.complete(),
@@ -52,10 +57,8 @@ local M = {
                 ["<Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() and has_words_before() then
                         cmp.select_next_item({ behavior = cmp.SelectBehavior.Replace })
-                    -- elseif luasnip.expand_or_locally_jumpable() then
-                    --     luasnip.expand_or_jump()
-                    -- elseif has_words_before() then
-                    --     cmp.complete()
+                    elseif luasnip.expand_or_locally_jumpable() then
+                        luasnip.expand_or_jump()
                     else
                         fallback()
                     end
@@ -63,19 +66,19 @@ local M = {
                 ["<S-Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_prev_item()
-                    -- elseif luasnip.jumpable( -1) then
-                    --     luasnip.jump( -1)
+                    elseif luasnip.jumpable( -1) then
+                        luasnip.jump( -1)
                     else
                         fallback()
                     end
                 end, { "i", "s" }),
             }),
             sources = {
-                { name = "nvim_lsp", priority = 1000 },
-                -- { name = "luasnip", priority = 800 },
-                { name = "copilot", priority = 700 },
-                { name = "path", priority = 600 },
-                { name = "buffer", priority = 500 },
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+                { name = "copilot" },
+                { name = "path" },
+                { name = "buffer" },
             },
             window = {
                 completion = cmp.config.window.bordered(),
