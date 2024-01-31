b8e40a1728cb6ac383998502b1a3694c649168da
ID-B3 <58172188+id-b3@users.noreply.github.com>
2024-01-31 13:35:21 +0000

    Further dap bindings

--- .config/nvim/lua/dudzie/plugins/nvim-dap-python.lua
+++ .config/nvim/lua/dudzie/plugins/nvim-dap-python.lua
@@ -9,6 +9,6 @@ return {
     keys = {
         {mode = "n", "<leader>duc", function() require('dap-python').test_class() end, silent = true, noremap = true, desc = "[D]AP [U]nittest [C]lass"},
         {mode = "n", "<leader>dum", function() require('dap-python').test_method() end, silent = true, noremap = true, desc = "[D]AP [U]nittest [M]ethod"},
-        {mode = "n", "<leader>dus", function() require('dap-python').debug_selection() end, silent = true, noremap = true, desc = "[D]AP [U]nittest [S]election"},
+        {mode = "v", "<leader>dus", function() require('dap-python').debug_selection() end, silent = true, noremap = true, desc = "[D]AP [U]nittest [S]election"},
     }
 }
--- .config/nvim/lua/dudzie/plugins/nvim-dap.lua
+++ .config/nvim/lua/dudzie/plugins/nvim-dap.lua
@@ -12,6 +12,7 @@ return {
         {mode = "n", "<F8>", function() require("dap").step_over() end, desc = "DAP Step Over"},
         {mode = "n", "<F5>", function() require("dap").step_into() end, desc = "DAP Step Into"},
         {mode = "n", "<F4>", function() require("dap").step_out() end, desc = "DAP Step Out"},
+        {mode = "n", "<F7>", function() require("dap").terminate() end, desc = "DAP Terminate" },
         {mode = "n", "<F12>", function() require("dap.ext.vscode").load_launchjs() end, desc = "Refresh launch.json"},
     },
 
