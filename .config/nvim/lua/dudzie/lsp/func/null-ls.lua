06050800f9fa0fd9dc7de98f0d49857b1fcd3f3a
id-b3 <58172188+id-b3@users.noreply.github.com>
2025-06-07 11:31:35 +0100

    New treesitter

--- .config/nvim/lua/dudzie/lsp/func/null-ls.lua
+++ .config/nvim/lua/dudzie/lsp/func/null-ls.lua
@@ -8,7 +8,7 @@ function M.setup()
     local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
     require('null-ls').setup({
         on_attach = function(client, bufnr)
-            if client.supports_method("textDocument/formatting") then
+            if client:supports_method("textDocument/formatting") then
                 vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
                 vim.api.nvim_create_autocmd("BufWritePre", {
                     group = augroup,
--- .config/nvim/lua/dudzie/lsp/func/utils.lua
+++ .config/nvim/lua/dudzie/lsp/func/utils.lua
@@ -10,10 +10,10 @@ M.lsp_servers = {
 -- Mason packages to install
 M.mason_packages = {
     "bash-language-server",
-    "black",
     "lua-language-server",
     "markdownlint",
-    "pyright"
+    "pyright",
+    "ruff",
 }
 
 -- Mason configuration
@@ -55,11 +55,6 @@ function M.get_capabilities()
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities.textDocument.completion.completionItem.snippetSupport = true
     return require("cmp_nvim_lsp").default_capabilities(capabilities)
-end
-
--- On attach function for LSP servers
-function M.on_attach(client, bufnr)
-    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 end
 
 return M
