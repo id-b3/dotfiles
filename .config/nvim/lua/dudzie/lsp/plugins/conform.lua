336bc7ed919add6c07ea6c6b5f540f2e5d13f034
id-b3 <58172188+id-b3@users.noreply.github.com>
2026-07-10 09:16:24 +0100

    Fix Python save collisions: unify formatting through conform, remove duplicate BufWritePre autocmd

--- .config/nvim/lua/dudzie/lsp/plugins/conform.lua
+++ .config/nvim/lua/dudzie/lsp/plugins/conform.lua
@@ -6,14 +6,17 @@ return {
         opts = {
             formatters_by_ft = {
                 lua = { "stylua" },
-                python = { "ruff_format" },
+                python = { "ruff_format", "ruff_fix" },
                 cpp = { "clang_format" },
                 json = { "prettier" },
                 markdown = { "prettier" },
             },
             format_on_save = {
-                timeout_ms = 500,
-                lsp_fallback = false,
+                timeout_ms = 1000,
+                lsp_fallback = "never",
+            },
+            default_format_opts = {
+                quiet = true,
             },
         },
     },
